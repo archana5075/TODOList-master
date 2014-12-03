e94cd4007c76bef58e267453c85902e4099b6be9
archana5075 <potnis.archana@gmail.com>
2014-12-03 15:08:52 -0800

    changed title name

--- app/js/services.js
+++ app/js/services.js
@@ -39,14 +39,14 @@ angular.module('myApp.services', [])
 	   var authServiceObject = {
 	   	   register: function(user){
 	   	   	auth.$createUser(user.email, user.password).then(function(data){
-               console.log(data);
+               //console.log(data);
                emails.$add({email: user.email});
                authServiceObject.login(user);
 	   	   	});
 	   	   }, 
 	       login: function(user){
            auth.$login('password', user).then(function(data){
-           console.log(data);
+           //console.log(data);
            $location.path('/todolist');
        });
     },
